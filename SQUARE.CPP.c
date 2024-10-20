4c28b5efb2b3055690e07db058b2dc770e755504
Asif Ali <jamaliasif637@gmail.com>
2024-10-20 14:14:50 +0500

    Add files via upload

---
+++ SQUARE.CPP.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int jamali;
+	printf("enter any num for jamali");
+	scanf("%d",&jamali);
+	printf("square is %d",jamali*jamali);
+	
+	getch();
+}
